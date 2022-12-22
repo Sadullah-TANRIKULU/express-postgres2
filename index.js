ffdfa256c2e6403fa62b75b5482ecdd643c1f8b1
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-22 23:42:57 +0300

    helperjs file deleted and indexjs redesigned

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const db = require("./helper");
 
 // this will create a server
 app.use(express.urlencoded({
@@ -24,11 +23,105 @@ app.use((req, res, next) => {
 
 
 // this will create a note in db
-app.get("/notes", db.getNotes);
-app.get("/note/:id", db.getNoteById);
-app.post("/note", db.createNote);
-app.put("/note/:id", db.updateNoteById);
-app.delete("/note/:id", db.deleteNote);
+app.get("/notes", (req, res) => {
+  try {
+      client.query("SELECT * FROM notes", (err, data) => {
+          if (err) throw err;
+
+          res.status(200).json({
+              err: null,
+              notes: data.rows,
+
+          });
+      });
+  } catch (error) {
+      res.status(500).json({
+          err: error.message,
+          notes: null,
+      });
+  }
+});
+
+app.get("/note/:id", (req, res) => {
+  try {
+      const id = req.params.id;
+      client.query("SELECT * FROM notes WHERE id = $1", [id], (err, data) => {
+          if (err) throw err;
+          
+          res.status(200).json({
+              err: null,
+              note: data.rows[0],
+          });
+      });
+  } catch (error) {
+      res.status(500).json({
+          err: err.message,
+          note: null,
+      });
+  }
+});
+app.post("/note", (req, res) => {
+  try {
+      const { note } = req.body;
+      if(!note) {
+          throw Error("Send note in request body");
+      }
+      client.query(
+          "INSERT INTO notes (note) VALUES ($1)", [note], 
+          (err, data) => {
+              res.status(201).json({
+                  error: null,
+                  message: "Created new note",
+              });
+          }
+      );
+  } catch (error) {
+      res.status(500).json({
+          error: error.message,
+          message: "Failed to create new note",
+      });
+  }
+});
+app.put("/note/:id", (req, res) => {
+  try {
+      const id = req.params.id;
+      const note = req.body.note;
+
+      client.query("UPDATE notes SET note = $1 WHERE id = $2",
+      [note, id],
+      (err, data) => {
+          if (err) throw err;
+
+          res.status(201).json({
+              err: null,
+              message: "Note updated",
+          });
+      });
+  } catch (error) {
+      res.status(500).json({
+          err: error.message,
+          message: "Failed to update note",
+      });
+  }
+});
+app.delete("/note/:id", (req, res) => {
+  try {
+      const id = req.params.id;
+      client.query("DELETE FROM notes WHERE id = $1", [id], 
+      (err, data) => {
+          if (err) throw err;
+          res.status(200).json({
+              err: null,
+              message: "Note deleted",
+          });
+      });
+  } catch (error) {
+      res.status(500).json({
+          err: error.message,
+          message: "Failed to delete note",
+      });
+  }
+});
 app.get("*", (req, res) => {
   res.send('no routes specified with this name!!!')
 })
