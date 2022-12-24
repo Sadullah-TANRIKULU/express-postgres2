daf388c97afc87e0b129e722a57e5c30a051fb84
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-24 17:15:05 +0300

    hhhh

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: './.env'});
 
 module.exports = {
     user: "postgres",
-    host: "localhost",
+    host: "127.0.0.1",
     database: "withExpress2",
     password: process.env.PASSWORD,
     porte: 5432,
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config({path: './.env'});
 
-let port=process.env.PORT||3000
+let $port=process.env.PORT||3000
 
 
 // this will create a server
@@ -11,7 +11,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${port} `);
+    console.log(`App running on port ${$port} `);
 });
 
 // allowing multiple origins
