2fad478081a8d23de50410ecc15aa0893ff2a927
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-24 15:16:13 +0300

    env reconfigured

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
+require('dotenv').config({path: './.env'});
 
 // this will create a server
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT, () => console.log(`Lstening on port ${process.env.PORT}`));
 
 // allowing multiple origins
 app.use((req, res, next) => {
