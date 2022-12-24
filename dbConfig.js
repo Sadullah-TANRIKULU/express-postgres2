018bd0cf6b5199ffc4dadd081581c9538926272b
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-24 15:39:20 +0300

    db listening port is changed

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: './.env'});
 
 module.exports = {
     user: "postgres",
-    host: "localhost",
+    host: "0.0.0.0",
     database: "withExpress2",
     password: process.env.PASSWORD,
     port: 5432,
